ea9382ed71343f76668d581b1d1aa782d2572d51
floyd-sk <floyd.skakane@gmail.com>
2021-11-12 01:20:18 +0200

    Random Forest

--- utils/train_model.py
+++ utils/train_model.py
@@ -12,20 +12,20 @@
 # Dependencies
 import pandas as pd
 import pickle
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
 # Fetch training data and preprocess for modeling
-train = pd.read_csv('./data/df_train.csv')
+train = pd.read_csv('df_train.csv')
 
-y_train = train[['load_shortfall_3h']]
-X_train = train[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+train_y = train[['load_shortfall_3h']]
+train_X = train[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
 
 # Fit model
-lm_regression = LinearRegression(normalize=True)
+randomfor =RandomForestRegressor(max_depth=4, max_features="sqrt")
 print ("Training Model...")
-lm_regression.fit(X_train, y_train)
+randomfor = randomfor.fit(train_X,train_y)
 
 # Pickle model for use within our API
-save_path = '../assets/trained-models/load_shortfall_simple_lm_regression.pkl'
+save_path = 'assets/trained-models/randomfor_model.pkl'
 print (f"Training completed. Saving model to: {save_path}")
-pickle.dump(lm_regression, open(save_path,'wb'))
+pickle.dump(randomfor, open(save_path,'wb'))
